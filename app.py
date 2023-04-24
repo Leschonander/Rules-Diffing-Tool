c2f715b10b31eaa6f7164b7aadf6b950ec897e92
Lars Schonander <lars@Larss-MacBook-Air.local>
2023-04-24 11:58:03 -0400

    open ai added

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import pandas as pd
+import openai
+import os
 from flask import Flask, jsonify, request, render_template
 
 app = Flask(__name__)
-
+openai.api_key = os.getenv("OPENAI_API_KEY")
 RulesReformSheet = pd.read_csv("Rules_combined_df_master.csv", lineterminator='\n') # pd.read_csv("Rules Reform Scraping Freshsheet - Sheet1 - Combined.csv")
 
 
@@ -47,6 +49,41 @@ def api_route():
     result = {"Result": list(RulesReformSheet["Text"])[0]} 
     
     return jsonify(result)
+
+@app.route('/api/openai')
+def openapi_api_route():
+    old_rule = request.args.get('rule_1')
+    new_rule = request.args.get('rule_2')
+
+    congress_1 = request.args.get('congress_1')
+    congress_2 = request.args.get('congress_2')
+
+    rule_title = request.args.get('rule_title')
+
+    '''
+    print(congress_1)
+    print(old_rule)
+    print("---")
+    print(congress_2)
+    print(new_rule)
+    print("---")
+    print(rule_title)
+    '''
+
+    completion = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo",
+        temperature = 0.8,
+        max_tokens = 2000,
+        messages = [
+        {"role": "system", "content": f"Explain the differences between the two following sets of rules for the {rule_title}. The first is from the {congress_1} congress , the second is from the {congress_2} Congress. Do not note changes in formatting."},
+        {"role": "system", "content": old_rule},
+        {"role": "system", "content": new_rule},
+        ]
+    )
+
+    result = {"Result": completion.choices[0].message["content"]} 
+
+    return jsonify(result) 
 
 if __name__ == '__main__':
     app.run(host="localhost", port=3000, debug=True)
